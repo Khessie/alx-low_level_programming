cf72f84aee41e8ffe32fb1dfc541d33c8cbead0a
Khessie <khessieevans@yahoo.com>
2022-04-07 14:31:58 -0700

    Task 4 edited again

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
-
+#include <stdio.h>
 /**
  * main - main block
  * Return: 0
  */
 int main(void)
 {
-putchar("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
-
