738d32e1dcc59ac402f8ef517fd0afa6e7feaac4
Khessie <khessieevans@yahoo.com>
2022-04-07 14:13:32 -0700

    Task 4 edited

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
+
 /**
- *main - main block
- *Return: 0 
-*/
-int main (void)
+ * main - main block
+ * Return: 0
+ */
+int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
+
