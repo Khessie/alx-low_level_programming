363556bba60847773f9257f9cabc923423730d49
Khessie <khessieevans@yahoo.com>
2022-04-12 04:43:30 -0700

    Advanced task 1

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,13 +3,12 @@
 /**
  * print_times_table - prints the n times table, starting with 0
  * @n: number of the times table
+ * Return: 0
  */
 void print_times_table(int n)
 {
-	print_times_table(2);
-	return (0);
-}
 int i, j, k;
+}
 
 if (n >= 0 && n <= 15)
 {
