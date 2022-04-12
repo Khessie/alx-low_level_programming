ab079d2589ee9c0cf2d0c52f245f69611fb2f39d
Khessie <khessieevans@yahoo.com>
2022-04-12 04:50:45 -0700

    Advanced task 1

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,49 +1,63 @@
 #include "main.h"
 
+int _putchar(char c);
+
 /**
- * print_times_table - prints the n times table, starting with 0
- * @n: number of the times table
- * Return: 0
+ * print_times_table - print the times table until n
+ *
+ * @n: n times table
+ *
+ * Return: No return
  */
+
 void print_times_table(int n)
 {
-int i, j, k;
-}
+int i;
+int j;
 
 if (n >= 0 && n <= 15)
 {
+
 for (i = 0; i <= n; i++)
 {
+
 for (j = 0; j <= n; j++)
 {
-k = j * i;
-if (j == 0)
+
+if ((i * j) < 10)
 {
-_putchar(k + '0');
-} else if (k < 10 && j != 0)
+if (j != 0)
 {
-_putchar(',');
 _putchar(' ');
 _putchar(' ');
 _putchar(' ');
-_putchar(k + '0');
-} else if (k >= 10 && k < 100)
-{
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar((k / 10) + '0');
-_putchar((k % 10) + '0');
-} else if (k >= 100)
-{
-_putchar(',');
-_putchar(' ');
-_putchar((k / 100) + '0');
-_putchar(((k / 10) % 10) + '0');
-_putchar((k % 10) + '0');
 }
+_putchar((j * i) + '0');
+}
+
+else if ((i * j) < 100)
+{
+_putchar(' ');
+_putchar(' ');
+_putchar((((j * i) / 10) % 10) + '0');
+_putchar(((j * i) % 10) + '0');
+}
+
+else if ((i * j) < 1000)
+{
+_putchar(' ');
+_putchar((((j * i) / 100) % 10) + '0');
+_putchar((((j * i) / 10) % 10) + '0');
+_putchar(((j * i) % 10) + '0');
+}
+if (j != (n))
+{
+_putchar(',');
+}
+
 }
 _putchar('\n');
 }
 }
+
 }
